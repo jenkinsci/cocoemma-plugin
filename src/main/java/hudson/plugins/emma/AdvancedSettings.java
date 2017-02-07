8a8b9e263c70e3cfa92606191a8d842fc4625c2e
Sebastien Fricker <fricker@froglogic.com>
2017-02-07 15:28:18 +0100

    Coverage method names are no more serialized.

--- src/main/java/hudson/plugins/emma/AdvancedSettings.java
+++ src/main/java/hudson/plugins/emma/AdvancedSettings.java
@@ -9,15 +9,6 @@ public class AdvancedSettings implements Serializable {
     
     private boolean testNotMandatory = false;
 
-    private String classDataColumnDescriptor = "";
-    private String methodDataColumnDescriptor = "";
-    private String blockDataColumnDescriptor = "";
-    private String lineDataColumnDescriptor = "";
-    private String decisionDataColumnDescriptor = "";
-    private String conditionDataColumnDescriptor = "";
-    private String mcdcDataColumnDescriptor = "";
-    private String mccDataColumnDescriptor = "";
-
 
     private String getValue(String value, String defaultValue){   
         
@@ -41,96 +32,52 @@ public class AdvancedSettings implements Serializable {
     
     public String getClassDataColumnDescriptor() {   
         
-        return getValue(classDataColumnDescriptor, Messages.CoverageObject_Legend_Class());
+        return Messages.CoverageObject_Legend_Class();
     }
-    
-    public void setClassDataColumnDescriptor(String name) {
-        
-        classDataColumnDescriptor = name;
-    }
+
 
     public String getMethodDataColumnDescriptor() {
         
-        return getValue(methodDataColumnDescriptor, Messages.CoverageObject_Legend_Block());
+        return Messages.CoverageObject_Legend_Method();
     }
     
-    public void setMethodDataColumnDescriptor(String name) {
-        
-        methodDataColumnDescriptor = name;
-    }
-
+ 
     public String getBlockDataColumnDescriptor() {
         
-        return getValue(blockDataColumnDescriptor, Messages.CoverageObject_Legend_Method());
+        return Messages.CoverageObject_Legend_Block();
     }
     
-    public void setBlockDataColumnDescriptor(String name) {
+     public String getLineDataColumnDescriptor() {
         
-        blockDataColumnDescriptor = name;
-    }
-
-    public String getLineDataColumnDescriptor() {
-        
-        return getValue(lineDataColumnDescriptor, Messages.CoverageObject_Legend_Line());
+        return Messages.CoverageObject_Legend_Line();
     }
     
-    public void setLineDataColumnDescriptor(String name) {
-        
-        lineDataColumnDescriptor = name;
-    }
-
     public String getConditionDataColumnDescriptor() {
         
-        return getValue(conditionDataColumnDescriptor, Messages.CoverageObject_Legend_Condition());
-    }
-    
-    public void setConditionDataColumnDescriptor(String name) {
-        
-        conditionDataColumnDescriptor = name;
+        return Messages.CoverageObject_Legend_Condition();
     }
     
     public String getDecisionDataColumnDescriptor() {
         
-        return getValue(decisionDataColumnDescriptor, Messages.CoverageObject_Legend_Decision());
-    }
-    
-    public void setDecisionDataColumnDescriptor(String name) {
-        
-        decisionDataColumnDescriptor = name;
+        return Messages.CoverageObject_Legend_Decision();
     }
     
     public String getMcDcDataColumnDescriptor() {
         
-        return getValue(mcdcDataColumnDescriptor, Messages.CoverageObject_Legend_Mcdc());
-    }
-    
-    public void setMccDataColumnDescriptor(String name) {
-        
-        mccDataColumnDescriptor = name;
+        return Messages.CoverageObject_Legend_Mcdc();
     }
     
     public String getMccDataColumnDescriptor() {
         
-        return getValue(mccDataColumnDescriptor, Messages.CoverageObject_Legend_Mcc());
+        return  Messages.CoverageObject_Legend_Mcc();
     }
     
-    public void setMcDcDataColumnDescriptor(String name) {
-        
-        mcdcDataColumnDescriptor = name;
-    }
+
     
     public void applySettings(AdvancedSettings settings){
         
         if(settings != null){
             setTestNotMandatory(settings.getTestNotMandatory());
-            setClassDataColumnDescriptor(settings.getClassDataColumnDescriptor());
-            setMethodDataColumnDescriptor(settings.getMethodDataColumnDescriptor());
-            setBlockDataColumnDescriptor(settings.getBlockDataColumnDescriptor());
-            setLineDataColumnDescriptor(settings.getLineDataColumnDescriptor());
-            setConditionDataColumnDescriptor(settings.getConditionDataColumnDescriptor());
-            setDecisionDataColumnDescriptor(settings.getDecisionDataColumnDescriptor());
-            setMcDcDataColumnDescriptor(settings.getMcDcDataColumnDescriptor());
-            setMccDataColumnDescriptor(settings.getMccDataColumnDescriptor());
         }
     }    
 }
