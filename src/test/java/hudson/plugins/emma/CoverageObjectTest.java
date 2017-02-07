88c3d998daa3a700b41cb98ed0cdf86a98e49c1d
Sebastien Fricker <fricker@froglogic.com>
2017-02-07 17:06:29 +0100

    Localization issue fixed in the test files.

--- src/test/java/hudson/plugins/emma/CoverageObjectTest.java
+++ src/test/java/hudson/plugins/emma/CoverageObjectTest.java
@@ -14,15 +14,15 @@ public class CoverageObjectTest extends AbstractEmmaTestBase {
     	r = new Ratio(0,100);
     	b = new StringBuilder();
     	CoverageObject.printRatioTable(r, b, no_tests_required);
-    	assertEquals("<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>0.0%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 0.0px;'><span class='text'>0/100</span></div></div></td></tr></table>", b.toString());
-//[KB-]    	assertEquals("<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>0,0%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 0.0px;'><span class='text'>0/100</span></div></div></td></tr></table>", b.toString());
-
+        String expected = "<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>0.0%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 0.0px;'><span class='text'>0/100</span></div></div></td></tr></table>";
+        String str = b.toString().replace( ",", ".");
+    	assertEquals(str,expected);
     	r = new Ratio(51,200);
     	b = new StringBuilder();
     	CoverageObject.printRatioTable(r, b, no_tests_required);
-    	assertEquals("<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>25.5%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 25.5px;'><span class='text'>51/200</span></div></div></td></tr></table>", b.toString());
-//[KB-]    	assertEquals("<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>25,5%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 25.5px;'><span class='text'>51/200</span></div></div></td></tr></table>", b.toString());
-
+    	expected = "<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>25.5%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 25.5px;'><span class='text'>51/200</span></div></div></td></tr></table>";
+        str = b.toString().replace(",", ".");
+    	assertEquals(str,expected);
     }
 
 
@@ -48,8 +48,11 @@ public class CoverageObjectTest extends AbstractEmmaTestBase {
     	r = new Ratio(51,200);
     	b = new StringBuilder();
     	CoverageObject.printRatioCell(false, r, b, no_tests_required);
-    	assertEquals("<td class='nowrap' data='025.50'>\n" +
-    			"<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>25.5%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 25.5px;'><span class='text'>51/200</span></div></div></td></tr></table></td>\n", b.toString());
+        String str = b.toString().replace( ",", "." );
+        String expected = "<td class='nowrap' data='025.50'>\n" +
+    			"<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>25.5%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 25.5px;'><span class='text'>51/200</span></div></div></td></tr></table></td>\n";
+
+    	assertEquals( str, expected );
 //[KB-]    	assertEquals("<td class='nowrap' data='025,50'>\n" +
 //[KB-]    			"<table class='percentgraph' cellpadding='0px' cellspacing='0px'><tr class='percentgraph'><td width='64px' class='data'>25,5%</td><td class='percentgraph'><div class='percentgraph'><div class='greenbar' style='width: 25.5px;'><span class='text'>51/200</span></div></div></td></tr></table></td>\n", b.toString());
 
