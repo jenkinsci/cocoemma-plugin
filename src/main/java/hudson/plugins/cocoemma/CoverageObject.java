a1032aa6b7f975a89ff8715b5b7b35402c3c4d7c
Adakar <jakub.top@gmail.com>
2022-04-12 15:59:38 +0200

    Parse strings to primitive and not objects
    In these cases, what we are really interested in are primitive types and
    not their object representations.

--- src/main/java/hudson/plugins/cocoemma/CoverageObject.java
+++ src/main/java/hudson/plugins/cocoemma/CoverageObject.java
@@ -219,8 +219,8 @@ public abstract class CoverageObject<SELF extends CoverageObject<SELF>> extends 
 
         String w = Util.fixEmptyAndTrim(req.getParameter("width"));
         String h = Util.fixEmptyAndTrim(req.getParameter("height"));
-        int width = (w != null) ? Integer.valueOf(w) : 500;
-        int height = (h != null) ? Integer.valueOf(h) : 200;
+        int width = (w != null) ? Integer.parseInt(w) : 500;
+        int height = (h != null) ? Integer.parseInt(h) : 200;
 
         new GraphImpl(this, t, width, height) {
 
